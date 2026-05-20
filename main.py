3c44f002094825e0c98e89d8c21283bbf7e34efd
Camiloc19 <jcruzvelrno@gmail.com>
2026-05-20 10:49:41 -0500

    actualizacion

--- main.py
+++ main.py
@@ -3,22 +3,14 @@ import mysql.connector
 conexion = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",
-    database="basedatos_prueba"
+    password="1234",
+    database="taller_el_costa"
 )
 
 print("Conectado")
 
 cursor = conexion.cursor()
+cursor.execute("select * from vehiculos")
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS estudiantes (
-    id_estudiantes INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(20) NOT NULL,
-    apellido VARCHAR(20) NOT NULL,
-    correo VARCHAR(50) NOT NULL,
-    edad INT NOT NULL
-)
-""")
-
-print("Tabla creada")
+for fila in cursor:
+    print(fila)
