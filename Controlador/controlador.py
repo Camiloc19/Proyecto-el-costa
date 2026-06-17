dd4c92a5ff0e50f388b7c05019e34d90f02ba982
angietorralba05 <angie.torralba05@gmail.com>
2026-06-17 10:05:37 -0500

    guardar roles en el login

--- Controlador/controlador.py
+++ Controlador/controlador.py
@@ -25,6 +25,7 @@ def login():
         usuario    = modelo.login_usuario(correo, contrasena)
         if usuario:
             session['usuario'] = usuario['nombre'] + ' ' + usuario['apellido']
+            session['id_rol']  = usuario['id_rol']     
             return redirect(url_for('dashboard'))
         else:
             return render_template('login.html', error='Correo o contraseña incorrectos')
